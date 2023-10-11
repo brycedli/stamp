1dd89b7b57c50b524b980bf8ff1808d4e0d8c4f1
brycedli <bryce.d.li@gmail.com>
2023-10-11 00:15:51 -0400

    Testing stamp differentiation

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
 let stamps = [];
-
+let currentStampIndex;
 function setup() {
     // set canvas to window size
     createCanvas(windowWidth, windowHeight);
@@ -20,7 +20,9 @@ function getTransform () {
     else{
         return [];
     }
-    return [touches[0].x + xDiff/2, touches[0].y + yDiff/2, 50, getTouchAngle(xDiff, yDiff)];
+
+    return [touches[0].x + xDiff/2, touches[0].y + yDiff/2, 50, getTouchAngle(xDiff, yDiff), sqrt(sq(xDiff) + sq(yDiff))];
+    //     0                        1                       2   3                            4
 }
 
 function getTouchAngle(xDiff,yDiff) {
@@ -37,17 +39,43 @@ function getTouchAngle(xDiff,yDiff) {
 function touchMoved () {
     if (stamps.length > 0){
         let t = getTransform();
-        stamps[stamps.length-1].updateTransform(t[0], t[1], t[2], t[3]);
+        let closestIndex = findClosestStampIndex (stamps, t[4], 10);
+        if (closestIndex == -1) {
+            stamps[stamps.length-1].updateTransform(t[0], t[1], t[2], t[3], t[4]);
+        }
+        else{
+            stamps[closestIndex].updateTransform(t[0], t[1], t[2], t[3], t[4]);
+        }
+        
     }
-    
 }
 
-
+function findClosestStampIndex (_stamps, distance, threshold) {
+    let closestDifference = threshold;
+    let closestStampIndex = -1;
+    for (let i = 0; i < _stamps.length; i++){
+        let currStamp = _stamps[i];
+        let diff = abs(currStamp.distance - distance);
+        print(currStamp);
+        if (diff < closestDifference) {
+            closestDifference = diff;
+            closestStampIndex = i;
+        }
+    }
+    return closestStampIndex;
+}
 
 function touchStarted () {
-    print(stamps);
     let t = getTransform();
-    stamps.push(new Stamp (t[0], t[1], t[2], t[3]));
+    if (touches.length == 2 || (touches.length == 1 && keyIsDown(32))){
+        let closestIndex = findClosestStampIndex (stamps, t[4], 10);
+        print(closestIndex);
+        if (closestIndex == -1){
+            currentStampIndex = stamps.push(new Stamp (t[0], t[1], t[2], t[3], t[4]));
+        }
+        
+    }
+    
     
 }
 
@@ -68,14 +96,15 @@ function draw() {
 }
 
 class Stamp {
-    constructor (x, y, size, rotation){
-        this.updateTransform(x, y, size, rotation);
+    constructor (x, y, size, rotation, distance){
+        this.updateTransform(x, y, size, rotation, distance);
     }
-    updateTransform (x, y, size, rotation) {
+    updateTransform (x, y, size, rotation, distance) {
         this.x = x;
         this.y = y;
         this.size = size;
         this.rotation = rotation;
+        this.distance = distance;
     }
     drawStamp () {
         push();
